fde13fe2a1c997ec49480becc4d19a2146c01c17
Cees-Jan Kiewiet <ceesjank@gmail.com>
2022-08-10 16:28:25 +0200

    Update to PHP 8.1

--- src/Serialize.php
+++ src/Serialize.php
@@ -1,95 +1,146 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace WyriHaximus\React\Cache;
 
 use React\Cache\CacheInterface;
-use React\Promise\PromiseInterface;
+
+use function serialize;
+use function unserialize;
 
 final class Serialize implements CacheInterface
 {
-    /** @var CacheInterface */
-    private $cache;
-
-    /**
-     * @param CacheInterface $cache
-     */
-    public function __construct(CacheInterface $cache)
+    public function __construct(private readonly CacheInterface $cache)
     {
-        $this->cache = $cache;
     }
 
     /**
-     * @param  string           $key
-     * @param  null             $default
-     * @return PromiseInterface
+     * @inheritDoc
+     * @phpstan-ignore-next-line
      */
     public function get($key, $default = null)
     {
-        return $this->cache->get($key, $default)->then(function ($result) use ($default) {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         * @psalm-suppress MissingClosureParamType
+         */
+        return $this->cache->get($key, $default)->then(static function ($result) use ($default): mixed {
             if ($result === null || $result === $default) {
-                return $result;
+                return $default;
             }
 
-            return \unserialize($result);
+            /**
+             * @psalm-suppress MixedArgument
+             */
+            return unserialize($result);
         });
     }
 
     /**
-     * @param  string           $key
-     * @param  mixed            $value
-     * @param  null             $ttl
-     * @return PromiseInterface
+     * @inheritDoc
+     * @phpstan-ignore-next-line
      */
     public function set($key, $value, $ttl = null)
     {
-        return $this->cache->set($key, \serialize($value), $ttl);
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
+        return $this->cache->set($key, serialize($value), $ttl);
     }
 
     /**
-     * @param  string           $key
-     * @return PromiseInterface
+     * @inheritDoc
      */
     public function delete($key)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->cache->delete($key);
     }
 
+    /**
+     * @inheritDoc
+     * @phpstan-ignore-next-line
+     */
     public function getMultiple(array $keys, $default = null)
     {
-        return $this->cache->getMultiple($keys, $default)->then(function ($results) use ($default) {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         * @psalm-suppress MissingClosureParamType
+         */
+        return $this->cache->getMultiple($keys, $default)->then(static function ($results) use ($default) {
+            /**
+             * @psalm-suppress MixedAssignment
+             */
             foreach ($results as $key => $result) {
                 if ($result === null || $result === $default) {
                     continue;
                 }
 
-                $results[$key] = \unserialize($result);
+                /**
+                 * @psalm-suppress MixedAssignment
+                 * @psalm-suppress MixedArrayAssignment
+                 * @psalm-suppress MixedArrayOffset
+                 * @psalm-suppress MixedArgument
+                 */
+                $results[$key] = unserialize($result);
             }
 
             return $results;
         });
     }
 
+    /**
+     * @inheritDoc
+     * @phpstan-ignore-next-line
+     */
     public function setMultiple(array $values, $ttl = null)
     {
+        /**
+         * @psalm-suppress MixedAssignment
+         */
         foreach ($values as $key => $value) {
-            $values[$key] = \serialize($value);
+            $values[$key] = serialize($value);
         }
 
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->cache->setMultiple($values, $ttl);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function deleteMultiple(array $keys)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->cache->deleteMultiple($keys);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function clear()
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->cache->clear();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function has($key)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->cache->has($key);
     }
 }
